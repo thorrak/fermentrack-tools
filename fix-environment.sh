e25ffcd59bba138db12521d90efe94b5909ab331
Thorrak <thorrak@excelinspace.com>
2020-05-25 09:19:03 -0400

    Install numpy without using wheels (#41)

--- fix-environment.sh
+++ fix-environment.sh
@@ -169,7 +169,7 @@ createPythonVenv() {
   sudo -u fermentrack -H bash -c "source /home/fermentrack/venv/bin/activate && pip install --no-binary pyzmq pyzmq==19.0.1"
   # I explicitly want to install Circus first, as there are potential clashes with versioning on pyzmq
   sudo -u fermentrack -H bash -c "source /home/fermentrack/venv/bin/activate && /home/fermentrack/venv/bin/python3 -m pip install circus"
-  sudo -u fermentrack -H bash -c "source /home/fermentrack/venv/bin/activate && /home/fermentrack/venv/bin/python3 -m pip install numpy scipy pandas"
+  sudo -u fermentrack -H bash -c "source /home/fermentrack/venv/bin/activate && /home/fermentrack/venv/bin/python3 -m pip install --no-binary numpy numpy==1.18.4"
   echo
 }
 
