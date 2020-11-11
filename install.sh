d604e9c1f2ce4b9e7c9bee0780cc3b5306121db7
Thorrak <thorrak@excelinspace.com>
2020-11-11 08:07:44 -0500

    Remove auto-relaunch as sudo from install.sh (this one's for you @vanosg)

--- install.sh
+++ install.sh
@@ -178,23 +178,12 @@ verifyRunAsRoot() {
         printinfo "This script was launched as root. Continuing installation."
     else
         printinfo "This script was called without root privileges. It installs and updates several packages,"
-        printinfo "creates user accounts and updates system settings. To continue this script will now attempt"
-        printinfo "to use 'sudo' to relaunch itself as root. Please check the contents of this script for any"
+        printinfo "creates user accounts, and updates system settings. To continue this script will exit now"
+        printinfo "and must be relaunched by you using sudo. Please check the contents of this script for any"
         printinfo "concerns with this requirement. Please be sure to access this script from a trusted source."
         echo
-
-        if command -v sudo &> /dev/null; then
-            # TODO - Make this require user confirmation before continuing
-            printinfo "This script will now attempt to relaunch using sudo."
-            exec sudo bash "$0" "$@"
-            exit $?
-        else
-            printerror "The sudo utility does not appear to be available on this system, and thus installation cannot continue."
-            printerror "Please run this script as root and it will be automatically installed."
-            exit 1
-        fi
+        die "Script must be relaunched using sudo "
     fi
-    echo
 
 }
 
