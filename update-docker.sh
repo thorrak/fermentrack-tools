3a734e918341973547ff0d569fef4a95ae45849e
Thorrak <thorrak@excelinspace.com>
2020-11-14 08:30:51 -0500

    Add `docker network prune -f` to update-docker.sh

--- update-docker.sh
+++ update-docker.sh
@@ -2,11 +2,13 @@
 
 docker image pull jdbeeler/fermentrack
 docker-compose -f production.yml down
+
+# We're going to prune the networks here as the mdns-reflector launch script is going to try to determine the network
+# to bridge based on
+docker network prune -f
 docker-compose -f production.yml build
 docker-compose -f production.yml up -d
 
-# Clean up/delete any unused docker images
+# Clean up/delete any unused docker images & networks
 docker image prune -f
-
-
 
