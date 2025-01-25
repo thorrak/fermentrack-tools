5408611b25185c72571209196f5f1c589be9d178
John <thorrak@users.noreply.github.com>
2025-01-25 09:39:28 -0500

    Remove `docker-compose` usage in docker-update.sh

--- docker-update.sh
+++ docker-update.sh
@@ -35,17 +35,17 @@ done
 shift $((OPTIND-1))
 
 
-docker-compose pull
+docker compose pull
 docker image pull "jdbeeler/fermentrack:${DOCKER_IMAGE_TAG}"
-docker-compose down
+docker compose down
 
 # We're going to prune the networks here as the mdns-reflector launch script is going to try to determine the network
 # to bridge based on
 docker network prune -f
-docker-compose build
+docker compose build
 # Run migrate here to prevent a race condition with celerybeat
-docker-compose run --rm django python manage.py migrate
-docker-compose up -d
+docker compose run --rm django python manage.py migrate
+docker compose up -d
 
 # Clean up/delete any unused docker images & networks
 #docker image prune -f
